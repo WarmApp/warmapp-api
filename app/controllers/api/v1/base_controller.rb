a3dac287985f4fec3f7eae7f68eda72100ca4690
William Calderipe <wcalderipe@gmail.com>
2015-11-09 00:12:48 -0200

    Adds null session for protect_from_forgery

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::BaseController < ApplicationController
+  protect_from_forgery with: :null_session
+
   respond_to :json
 
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
