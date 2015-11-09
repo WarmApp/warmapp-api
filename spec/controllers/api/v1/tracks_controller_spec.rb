c35de8afe4f22227237945d6e0c6a2a9b8e410b5
William Calderipe <wcalderipe@gmail.com>
2015-11-09 00:36:50 -0200

    Adds late specs for tracks index and update action

--- spec/controllers/api/v1/tracks_controller_spec.rb
+++ spec/controllers/api/v1/tracks_controller_spec.rb
@@ -11,7 +11,7 @@ describe Api::V1::TracksController do
     it { should respond_with 200 }
 
     it 'shoud responde with 5 tracks' do
-      expect(json_responsep[:tracks].length).to eql 5
+      expect(json_response[:tracks].length).to eql 5
     end
   end
 
@@ -65,12 +65,27 @@ describe Api::V1::TracksController do
   end
 
   describe 'PATCH/PUT #update' do
-    context 'when is successfully updated' do
+    before :each do
+      @track = FactoryGirl.create(:track)
+    end
 
+    context 'when is successfully updated' do
+      before :each do
+        patch :update, { id: @track.id, track: { name: 'foo' } }, format: :json
+      end
+
+      it { should respond_with 200 }
+
+      it 'renders the json representation for the track updated' do
+        expect(json_response[:track][:name]).to eql 'foo'
+      end
     end
 
     context 'when is not updated' do
-
+      it 'should respond with 404 if track is not found' do
+        patch :update, { id: -1 }, format: :json
+        expect(response.status).to eql 404
+      end
     end
   end
 end
